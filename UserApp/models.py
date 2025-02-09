ae8521fdc83a65c2c8f69f5dba9493464c1c4040
d1yas <diyasdjumabaevv@gmail.com>
2025-02-09 18:44:15 +0500

    add User model

--- UserApp/models.py
+++ UserApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=18)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=9, unique=True)
+    password = models.CharField(max_length=16)
+
+
+    def __str__(self):
+        return self.username
